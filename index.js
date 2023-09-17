4ed1df84e6b11627f06d1ccbf767df67b0cdb4b3
b.wolska <beata.wolska@epikur.de>
2023-09-17 22:19:28 +0200

    a

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const user = require('./routes/user');
 const userCategoryStatus = require('./routes/user-category-status');
 const words = require('./routes/words');
 
-const index = express();
-index.use(require("cors")());
-index.use(require("body-parser").json());
+const app = express();
+app.use(require("cors")());
+app.use(require("body-parser").json());
 
 
 const uri = "mongodb+srv://beata111:wirowiro@cluster0.keyagdk.mongodb.net/?retryWrites=true&w=majority";
@@ -16,34 +16,38 @@ const uri = "mongodb+srv://beata111:wirowiro@cluster0.keyagdk.mongodb.net/?retry
 mongodb.MongoClient.connect(uri, (err, client) => {
   let db = client.db("languages_app_new");
 
-  index.get("/", (req, res) => {
+  app.get("/", (req, res) => {
     res.type("text/plain");
     res.status(200);
     res.send("Hallo");
   });
 
+  app.get('/home', (req, res) => {
+    res.status(200).json('Welcome, your app is working well');
+  })
+
   // auth
-  index.post("/api/create-user", auth.createUser(db));
-  index.post("/api/login-user", auth.loginUser(db));
+  app.post("/api/create-user", auth.createUser(db));
+  app.post("/api/login-user", auth.loginUser(db));
 
   // user
-  index.get("/api/get-user", user.getUser(db));
+  app.get("/api/get-user", user.getUser(db));
 
   // user-category-status
-  index.get("/api/get-category-status", userCategoryStatus.getUserCategoryStatus(db));
-  index.post("/api/update-category-status", userCategoryStatus.updateUserCategoryStatus(db));
+  app.get("/api/get-category-status", userCategoryStatus.getUserCategoryStatus(db));
+  app.post("/api/update-category-status", userCategoryStatus.updateUserCategoryStatus(db));
 
   // words
-  index.get("/api/get-words", words.getWords(db));
+  app.get("/api/get-words", words.getWords(db));
 
-  index.use((req, res) => {
+  app.use((req, res) => {
     res.type("text/plain");
     res.status(404);
     res.send("404 Not found ☕_☕");
   });
 
   // listen for requests
-  index.listen(process.env.PORT || 5678, () => {
+  app.listen(process.env.PORT || 5678, () => {
     console.log("Your app is listening on port ", process.env.PORT || 5678);
   });
 });
