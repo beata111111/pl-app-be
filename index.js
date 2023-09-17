13322ad33c5b666a73ed1cfea9b45fd5daadd42b
b.wolska <beata.wolska@epikur.de>
2023-09-17 22:09:57 +0200

    a

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const user = require('./routes/user');
 const userCategoryStatus = require('./routes/user-category-status');
 const words = require('./routes/words');
 
-const app = express();
-app.use(require("cors")());
-app.use(require("body-parser").json());
+const index = express();
+index.use(require("cors")());
+index.use(require("body-parser").json());
 
 
 const uri = "mongodb+srv://beata111:wirowiro@cluster0.keyagdk.mongodb.net/?retryWrites=true&w=majority";
@@ -16,34 +16,34 @@ const uri = "mongodb+srv://beata111:wirowiro@cluster0.keyagdk.mongodb.net/?retry
 mongodb.MongoClient.connect(uri, (err, client) => {
   let db = client.db("languages_app_new");
 
-  app.get("/", (req, res) => {
+  index.get("/", (req, res) => {
     res.type("text/plain");
     res.status(200);
     res.send("Hallo");
   });
 
   // auth
-  app.post("/api/create-user", auth.createUser(db));
-  app.post("/api/login-user", auth.loginUser(db));
+  index.post("/api/create-user", auth.createUser(db));
+  index.post("/api/login-user", auth.loginUser(db));
 
   // user
-  app.get("/api/get-user", user.getUser(db));
+  index.get("/api/get-user", user.getUser(db));
 
   // user-category-status
-  app.get("/api/get-category-status", userCategoryStatus.getUserCategoryStatus(db));
-  app.post("/api/update-category-status", userCategoryStatus.updateUserCategoryStatus(db));
+  index.get("/api/get-category-status", userCategoryStatus.getUserCategoryStatus(db));
+  index.post("/api/update-category-status", userCategoryStatus.updateUserCategoryStatus(db));
 
   // words
-  app.get("/api/get-words", words.getWords(db));
+  index.get("/api/get-words", words.getWords(db));
 
-  app.use((req, res) => {
+  index.use((req, res) => {
     res.type("text/plain");
     res.status(404);
     res.send("404 Not found ☕_☕");
   });
 
   // listen for requests
-  app.listen(process.env.PORT || 5678, () => {
+  index.listen(process.env.PORT || 5678, () => {
     console.log("Your app is listening on port ", process.env.PORT || 5678);
   });
 });
