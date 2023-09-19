f55334693fca8391d569c51f238ea618c590dd67
b.wolska <beata.wolska@epikur.de>
2023-09-19 22:06:25 +0200

    v

--- index.js
+++ index.js
@@ -13,14 +13,8 @@ app.use(require("body-parser").json());
 
 const uri = "mongodb+srv://beata111:wirowiro@cluster0.keyagdk.mongodb.net/?retryWrites=true&w=majority";
 
-console.log('aaa');
 mongodb.MongoClient.connect(uri, (err, client) => {
-  console.log('bbb');
-
   let db = client.db("languages_app_new");
-
-  console.log('ccc');
-
 
   app.get("/", (req, res) => {
     res.type("text/plain");
@@ -28,53 +22,29 @@ mongodb.MongoClient.connect(uri, (err, client) => {
     res.send("Hallo");
   });
 
-  // app.get('/home', (req, res) => {
-  //   res.status(200).json('Welcome, your app is working well');
-  // })
-  //
-  // // auth
-  // app.post("/api/create-user", auth.createUser(db));
-  // app.post("/api/login-user", auth.loginUser(db));
-  //
-  // // user
-  // app.get("/api/get-user", user.getUser(db));
-  //
-  // // user-category-status
-  // app.get("/api/get-category-status", userCategoryStatus.getUserCategoryStatus(db));
-  // app.post("/api/update-category-status", userCategoryStatus.updateUserCategoryStatus(db));
-  //
-  // // words
-  // app.get("/api/get-words", words.getWords(db));
-  //
-  // app.use((req, res) => {
-  //   res.type("text/plain");
-  //   res.status(404);
-  //   res.send("404 Not found ☕_☕");
-  // });
+  // auth
+  app.post("/api/create-user", auth.createUser(db));
+  app.post("/api/login-user", auth.loginUser(db));
+
+  // user
+  app.get("/api/get-user", user.getUser(db));
+
+  // user-category-status
+  app.get("/api/get-category-status", userCategoryStatus.getUserCategoryStatus(db));
+  app.post("/api/update-category-status", userCategoryStatus.updateUserCategoryStatus(db));
+
+  // words
+  app.get("/api/get-words", words.getWords(db));
+
+  app.use((req, res) => {
+    res.type("text/plain");
+    res.status(404);
+    res.send("404 Not found ☕_☕");
+  });
 
   // listen for requests
-  app.listen(5678, () => {
-    console.log("Your app is listening on port ", 5678);
+  app.listen(process.env.PORT || 5678, () => {
+    console.log("Your app is listening on port ", process.env.PORT || 5678);
   });
 });
 
-// // index.js
-// const express = require('express')
-//
-// const app = express()
-// const PORT = 4000
-//
-// app.listen(PORT, () => {
-//   console.log(`API listening on PORT ${PORT} `)
-// })
-//
-// app.get('/', (req, res) => {
-//   res.send('Hey this is my API running 🥳')
-// })
-//
-// app.get('/about', (req, res) => {
-//   res.send('This is my about route..... ')
-// })
-//
-// // Export the Express API
-// module.exports = app
